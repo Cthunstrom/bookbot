e02a300c291f1936df592a5b5e06b35e666e7349
cthunstrom <thunstromc@gmail.com>
2025-01-16 00:41:38 -0800

    v0.3 added count characters function

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ def main():
             file_contents = f.read()
 
     print(get_word_count(file_contents))
+    print(count_characters(file_contents))
 
 def get_word_count(words):
       return len(words.split())
+
+def count_characters(words):
+      character_list = {}
+      for word in words:
+            character = word.lower()
+            if character not in character_list:
+                  character_list[character] = 1
+            else:
+                  character_list[character] += 1
+      return(character_list)
 
 main()
